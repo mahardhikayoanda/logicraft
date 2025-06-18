ef1991604f75194097beb642be4575f9cf25fa0d
zamazenta <zama@gmail.com>
2025-06-18 17:36:15 +0700

    menambahkan halaman customer

--- Source2/homestay-app/resources/views/layouts/admin.blade.php
+++ Source2/homestay-app/resources/views/layouts/admin.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <title>Dashboard Admin</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
 </head>
 
 <body class="flex bg-gray-100 min-h-screen">
